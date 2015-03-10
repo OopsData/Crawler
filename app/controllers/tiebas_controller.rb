853b8313cb57138918084bc0f169575e409c7acb
Davis Gao <naitnix@126.com>
2015-03-10 16:04:13 +0800

    m

--- app/controllers/tiebas_controller.rb
+++ app/controllers/tiebas_controller.rb
@@ -1,4 +1,5 @@
 class TiebasController < ApplicationController
+	protect_from_forgery with: :null_session
 	def create
 		Rails.logger.info('=======================================')
 		Rails.logger.info(params.inspect)
