a2b1df22606576722a4a237f980d285302be3417
Davis Gao <naitnix@126.com>
2015-03-10 16:08:59 +0800

    m

--- app/controllers/tiebas_controller.rb
+++ app/controllers/tiebas_controller.rb
@@ -1,6 +1,8 @@
 class TiebasController < ApplicationController
 	protect_from_forgery with: :null_session
 	def create
+		rack_input = env["rack.input"].read
+      	params = Rack::Utils.parse_query(rack_input, "&")		
 		Rails.logger.info('=======================================')
 		Rails.logger.info(params.inspect)
 		Rails.logger.info('=======================================')
