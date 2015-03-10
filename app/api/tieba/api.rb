485f86d85c3fcdf2bb502bf441a5df85c8ca777e
Davis Gao <naitnix@126.com>
2015-03-10 18:41:14 +0800

    m

--- app/api/tieba/api.rb
+++ app/api/tieba/api.rb
@@ -1,15 +1,18 @@
 module Tieba
   class API < Grape::API
-    version 'v1', using: :header, vendor: 'twitter'
+    version 'v1', using: :header, vendor: 'tiebas'
     format :json
     prefix :api
 
 
     resource :tiebas do
+      get  :data do 
+        Rails.logger.info 'dddddddddddddddddd'
+      end
       post :receive_data do
-        puts '================================'
-        puts params.inspect
-        puts '================================'
+        Rails.logger.info '================================'
+        Rails.logger.info params.inspect
+        Rails.logger.info '================================'
       end
     end
   end
