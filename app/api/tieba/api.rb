c0497c1661308fb5f52960fa717fc6e2f1e4ec4c
Davis Gao <naitnix@126.com>
2015-03-11 09:04:37 +0800

    m

--- app/api/tieba/api.rb
+++ app/api/tieba/api.rb
@@ -10,17 +10,17 @@ module Tieba
         Rails.logger.info 'dddddddddddddddddd'
       end
       post :receive_data do
-        data_arr  = eval(params[:data])
+        #data_arr  = eval(params[:data])
         Rails.logger.info('=========================================')
-        Rails.logger.info data_arr.inspect
+        Rails.logger.info params[:data].inspect
         Rails.logger.info('=========================================')
-        data_arr.each do |hash|
-          puts hash[:title]
-          puts hash[:author]
-          puts hash[:created]
-          puts hash[:comment]
-          Rails.logger.info '----------------------------------------------------------------------------'
-        end
+        # data_arr.each do |hash|
+        #   puts hash[:title]
+        #   puts hash[:author]
+        #   puts hash[:created]
+        #   puts hash[:comment]
+        #   Rails.logger.info '----------------------------------------------------------------------------'
+        # end
       end
     end
   end
