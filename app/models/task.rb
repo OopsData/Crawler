bd564737257ab3ddf48211bcf95791075d6e2104
Davis Gao <naitnix@126.com>
2015-06-28 13:50:55 +0800

    debug

--- app/models/task.rb
+++ app/models/task.rb
@@ -343,7 +343,7 @@ class Task
 
   def self.generate_qqlive_excel
     td  = Date.today.strftime('%F')
-    datas = Qqlive.all.select{|e| e.time.strftime('%F') == Time.now.strftime('%F') }
+    datas = Qqlive.desc(:time).select{|e| e.time.strftime('%F') == Time.now.strftime('%F') }
     tmp_datas = []
     datas.each_slice(50000) do |qqlives|
       tmp_datas << qqlives
